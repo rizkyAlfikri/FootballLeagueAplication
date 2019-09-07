67cd3d71f43475b4fe0a73c2aad5e69a54714899
xzpwnz <rizkyalfikri@gmail.com>
2019-09-07 18:04:10 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/footballleagueaplication/LeagueAdapter.kt
@@ -0,0 +1,81 @@
+package com.dicoding.picodiploma.footballleagueaplication
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.view.View
+import android.view.View.TEXT_ALIGNMENT_CENTER
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.footballleagueaplication.models.LeagueModel
+import kotlinx.android.extensions.LayoutContainer
+
+
+import org.jetbrains.anko.*
+import org.jetbrains.anko.cardview.v7.cardView
+
+class LeagueAdapter(private val listLeague: List<LeagueModel>, private val listener: (LeagueModel) -> Unit ) :
+    RecyclerView.Adapter<LeagueAdapter.LeagueViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeagueViewHolder {
+        return LeagueViewHolder(LeagueUI().createView(AnkoContext.Companion.create(parent.context, parent)))
+    }
+
+    override fun getItemCount() = listLeague.size
+
+    override fun onBindViewHolder(holder: LeagueViewHolder, position: Int) {
+        holder.bindData(listLeague[position], listener)
+    }
+
+    class LeagueViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        private val txtName = containerView.findViewById<TextView>(R.id.txt_name)
+        private val imgPhoto = containerView.findViewById<ImageView>(R.id.img_photo)
+
+        fun bindData(listData: LeagueModel, listener: (LeagueModel) -> Unit) {
+            txtName.text = listData.leagueName
+            Glide.with(itemView).load(listData.leagueImage).into(imgPhoto)
+            containerView.setOnClickListener { listener(listData) }
+        }
+    }
+
+    class LeagueUI : AnkoComponent<ViewGroup> {
+
+        override fun createView(ui: AnkoContext<ViewGroup>) = with(ui) {
+            frameLayout {
+                cardView {
+                    background.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                    radius = dip(12).toFloat()
+
+                    relativeLayout {
+                        imageView {
+                            id = R.id.img_photo
+                        }.lparams(width = 300, height = 300) {
+                            centerHorizontally()
+                            topMargin = dip(16)
+                        }
+                        textView {
+                            textSize = sp(8).toFloat()
+                            textColor = R.color.secondary_text
+                            id = R.id.txt_name
+                            textAlignment = TEXT_ALIGNMENT_CENTER
+                        }.lparams(width = matchParent, height = wrapContent) {
+                            below(R.id.img_photo)
+                            horizontalMargin = dip(8)
+                            topMargin = dip(8)
+                            bottomMargin = dip(16)
+                            centerInParent()
+                        }
+                    }.lparams(width = matchParent, height = matchParent)
+
+                }.lparams(width = matchParent, height = matchParent) {
+                    margin = dip(12)
+                    elevation = dip(6).toFloat()
+                }
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/dicoding/picodiploma/footballleagueaplication/models/LeagueModel.kt
@@ -0,0 +1,9 @@
+package com.dicoding.picodiploma.footballleagueaplication.models
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class LeagueModel(val leagueName: String,val leagueDesc: String, val leagueId: String, val leagueImage: Int) : Parcelable {
+}
